140abd7aeaff01e7ab11c77b688a04892e24121b
RhuanJanau <rhuanjanau531@gmail.com>
2022-10-07 23:47:29 -0300

    feat: image

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:registration/src/app_module.dart';
 import 'package:registration/src/app_widgets.dart';
 
-void main(){
-  return runApp(ModularApp(module: AppModule(), child: AppWidget()));
+void main() {
+  return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
--- lib/src/modules/home/view/home_view.dart
+++ lib/src/modules/home/view/home_view.dart
@@ -5,9 +5,40 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Image.asset('assets/image/boll.png'),
+            Positioned.fill(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset('assets/image/logo.png'),
+                  ),
+                  const Text(
+                    'Get your buisness with MyApp',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 64.0),
+                    child: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate volutpat cursus euismod nunc sapien. Dictum id hac feugiat laoreet.',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
