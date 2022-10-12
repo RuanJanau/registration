dc6b054c2f94367a6f88d8eb24a94ef03d5d05b3
RhuanJanau <rhuanjanau531@gmail.com>
2022-10-12 03:38:50 -0300

    deleted paths

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
-
-import 'modules/login/view/login_view.dart';
+import 'package:registration/src/modules/home/view/home_view.dart';
+import 'package:registration/src/modules/login/login_module.dart';
 
 class AppModule extends Module {
   @override
@@ -8,6 +8,8 @@ class AppModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => const LoginView()),
+        ChildRoute(Modular.initialRoute,
+            child: ((context, args) => const HomeView())),
+        ModuleRoute('/login', module: LoginModule())
       ];
 }
