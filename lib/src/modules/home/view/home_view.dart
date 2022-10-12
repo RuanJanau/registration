d727779dfa47279d0ea38a974ba1b56f85499150
RhuanJanau <rhuanjanau531@gmail.com>
2022-10-12 02:37:08 -0300

    feat: ElevatedButton

--- lib/src/modules/home/view/home_view.dart
+++ lib/src/modules/home/view/home_view.dart
@@ -12,29 +12,47 @@ class HomeView extends StatelessWidget {
         child: Stack(
           children: [
             Image.asset('assets/image/boll.png'),
-            Positioned.fill(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Image.asset('assets/image/logo.png'),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 80),
+                Center(
+                  child: Image.asset('assets/image/logo.png'),
+                ),
+                const Text(
+                  'Get your buisness with MyApp',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                ),
+                const SizedBox(height: 20.0),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 64.0),
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate volutpat cursus euismod nunc sapien. Dictum id hac feugiat laoreet.',
+                    textAlign: TextAlign.center,
                   ),
-                  const Text(
-                    'Get your buisness with MyApp',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
-                  ),
-                  const SizedBox(height: 20.0),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 64.0),
-                    child: Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate volutpat cursus euismod nunc sapien. Dictum id hac feugiat laoreet.',
-                      textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 70),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .80,
+                      height: 66.0,
+                      child: ElevatedButton(
+                        onPressed: () => (() {}),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xff48C2DC),
+                          shape: const RoundedRectangleBorder(),
+                        ),
+                        child: const Text(
+                          'Get Started',
+                          style: TextStyle(
+                              fontSize: 22.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
-                  ),
-                  
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
